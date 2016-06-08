b5d28208f19c388cff44f02522407db693c5e6c7
Jonathan Hartman <j@p4nt5.com>
2016-06-08 08:05:53 -0700

    Update to mac-app-store 2.1.0

--- Policyfile.rb
+++ Policyfile.rb
@@ -2,5 +2,3 @@ name 'dev-machine' # rubocop:disable FileName
 default_source :supermarket
 run_list 'my-dev-machine'
 cookbook 'my-dev-machine', path: '.'
-
-default['mac_app_store']['username'] = 'j@p4nt5.com'
